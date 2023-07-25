c82dec72fabc19b1bae8a156aeec22e2b856ef15
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-25 14:28:36 +0300

    Starting the quiz app creation.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import DateCounter from "./components/DateCounter";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <DateCounter />
+      <Header />
     </div>
   );
 }
