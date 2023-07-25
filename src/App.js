484f8254677dd6cd0f73eb22fe11ca2350048494
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-07-25 17:16:20 +0300

    Handeling status and featching data and create start screen.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useEffect, useReducer, useState } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen;";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFaild":
+      return { ...state, status: "error" };
+    default:
+      return state;
+  }
+};
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((result) => result.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFaild" }));
+  }, []);
   return (
     <div className="app">
       <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
